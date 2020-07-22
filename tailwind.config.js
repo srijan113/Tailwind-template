6664a1e4d3fcb6e59b55a38773835ea9f10a667a
Srijan Pokhrel <srijan.pokhrel113@gmail.com>
2020-07-22 11:05:38 +0545

    Made config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#ff6363',
+        secondary:{
+          100:'#E2E2D5',
+          200:'#888883',
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
